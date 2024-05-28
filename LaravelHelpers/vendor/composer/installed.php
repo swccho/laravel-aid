ac0173f9479546cfc37db70d3ce81a998d535386
Ali Haider Swccho <swccho263@gmail.com>
2024-05-28 18:39:11 +0600

    init 2

---
+++ LaravelHelpers/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ahs-laravel/helpers',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6e139ea17e4143555dd2aedf02dc681ff3cd0dbf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ahs-laravel/helpers' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6e139ea17e4143555dd2aedf02dc681ff3cd0dbf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
